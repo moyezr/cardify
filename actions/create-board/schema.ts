55a50f4e819b4261bd98649d62010c655afd7615
Moyez <moyezrabbani@gmail.com>
2023-11-29 17:50:41 +0530

    built the base for query and mutation using server actions

---
+++ actions/create-board/schema.ts
@@ -0,0 +1,10 @@
+import {z} from "zod"
+
+const CreateBoard = z.object({
+    title: z.string({
+        required_error: "Title is required",
+        invalid_type_error: "Title must be a string"
+    }).min(3, {
+        message: "Title is too short"
+    })
+})
---
+++ components/form/form-input.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react'
+
+type Props = {}
+
+const FormInput = (props: Props) => {
+  return (
+    <div>FormInput</div>
+  )
+}
+
+export default FormInput
