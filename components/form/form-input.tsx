bfd27cef1751d11935e9c139555be284036c260d
Moyez <moyezrabbani@gmail.com>
2023-11-29 18:10:58 +0530

    added form components

--- components/form/form-input.tsx
+++ components/form/form-input.tsx
@@ -1,13 +1,73 @@
-"use client"
+"use client";
 
-import React from 'react'
+import React, { forwardRef } from "react";
+import { useFormStatus } from "react-dom";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
+import { cn } from "@/lib/utils";
+import FormErrors from "./form-errors";
 
-type Props = {}
-
-const FormInput = (props: Props) => {
-  return (
-    <div>FormInput</div>
-  )
+interface FormInputProps {
+  id: string;
+  label?: string;
+  type?: string;
+  placeholder?: string;
+  required?: boolean;
+  disabled?: boolean;
+  errors?: Record<string, string[] | undefined>;
+  className?: string;
+  defaultValue?: string;
+  onBlur?: () => void;
 }
+const FormInput = forwardRef<HTMLInputElement, FormInputProps>(
+  (
+    {
+      id,
+      label,
+      type,
+      placeholder,
+      required,
+      disabled,
+      errors,
+      className,
+      defaultValue = "",
+      onBlur,
+    },
+    ref
+  ) => {
+    const { pending } = useFormStatus();
 
-export default FormInput
+    return (
+      <div className="space-y-2">
+        <div className="space-y-1">
+          {label ? (
+            <Label
+              htmlFor={id}
+              className="text-xs font-semibold text-neutral-700"
+            >
+              {label}
+            </Label>
+          ) : null}
+          <Input
+            onBlur={onBlur}
+            defaultValue={defaultValue}
+            ref={ref}
+            required={required}
+            name={id}
+            id={id}
+            placeholder={placeholder}
+            type={type}
+            disabled={pending || disabled}
+            className={cn("text-sm py-1 h-7 px-2", className)}
+            aria-describedby={`${id}-error`}
+          />
+        </div>
+        <FormErrors id={id} errors={errors} />
+      </div>
+    );
+  }
+);
+
+FormInput.displayName = "FormInput";
+
+export default FormInput;
---
+++ components/form/form-submit.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { ReactNode } from "react";
+import { useFormState, useFormStatus } from "react-dom";
+import { Button } from "../ui/button";
+import { cn } from "@/lib/utils";
+
+interface FormSubmitProps {
+  children: ReactNode;
+  disabled?: boolean;
+  className?: string;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link"
+    | "primary";
+}
+
+const FormSubmit = ({
+  children,
+  disabled,
+  className,
+  variant = "primary",
+}: FormSubmitProps) => {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      disabled={pending || disabled}
+      type="submit"
+      variant={variant}
+      size="sm"
+      className={cn(className)}
+    >
+      {children}
+    </Button>
+  );
+};
+
+export default FormSubmit;
