4413940f57f1de38f249595e75fe098e2c2e6123
Moyez <moyezrabbani@gmail.com>
2023-11-30 16:30:24 +0530

    added ui for adding lists and db schema for cards and lists

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,9 +1,46 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import React from "react";
+import ListContainer from "./_components/list-container";
 
-type Props = {};
+interface BoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+}
 
-const BoardIdPage = async (props: Props) => {
-  return <div>BoardIdPage</div>;
+const BoardIdPage = async ({ params }: BoardIdPageProps) => {
+  const { orgId, userId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const lists = await db.list.findMany({
+    where: {
+      id: params.boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
+
+  return (
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer boardId={params.boardId} data={lists} />
+    </div>
+  );
 };
 
 export default BoardIdPage;
