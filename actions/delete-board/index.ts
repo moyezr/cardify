0a0efb9d09f96c4c90f303a3807b6068542f12a5
Moyez <moyezrabbani@gmail.com>
2023-12-02 16:02:13 +0530

    connected the stripe billing portal to the organization billing page

--- actions/delete-board/index.ts
+++ actions/delete-board/index.ts
@@ -12,6 +12,7 @@ import { InputType, ReturnType } from "./types";
 import { createAuditLog } from "@/lib/create-audit-log";
 import { ACTION, ENTITY_TYPE } from "@prisma/client";
 import { decreaseAvailableCount } from "@/lib/org-limit";
+import { checkSubscription } from "@/lib/subscription";
 // import { checkSubscription } from "@/lib/subscription";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
@@ -23,7 +24,7 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     };
   }
 
-//   const isPro = await checkSubscription();
+  const isPro = await checkSubscription();
 
   const { id } = data;
   let board;
@@ -36,7 +37,7 @@ const handler = async (data: InputType): Promise<ReturnType> => {
       },
     });
 
-    if (/*!isPro*/ true) {
+    if (!isPro) {
       await decreaseAvailableCount();
     }
 
---
+++ app/(platform)/(dashboard)/organization/[organizationId]/billing/page.tsx
@@ -0,0 +1,23 @@
+import { checkSubscription } from '@/lib/subscription'
+import React from 'react'
+import Info from '../_components/info';
+import { Separator } from '@/components/ui/separator';
+import SubscriptionButton from './_components/subscription-button';
+
+type Props = {}
+
+const BillingPage = async (props: Props) => {
+
+    const isPro = await checkSubscription();
+
+
+  return (
+    <div className='w-full'>
+        <Info isPro={isPro} />
+        <Separator className='my-2' />
+        <SubscriptionButton isPro={isPro} />
+    </div>
+  )
+}
+
+export default BillingPage
