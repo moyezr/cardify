82f332c2db9ee82e1e4a39781ccf184efef0bc23
Moyez <moyezrabbani@gmail.com>
2023-12-02 14:13:39 +0530

    added ui for activity tab in the organizations tab

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/activity/page.tsx
@@ -0,0 +1,20 @@
+import React, { Suspense } from "react";
+import Info from "../_components/info";
+import { Separator } from "@/components/ui/separator";
+import ActivityList from "./_components/activity-list";
+
+type Props = {};
+
+const ActivityPage = (props: Props) => {
+  return (
+    <div className="w-full">
+      <Info  />
+      <Separator className="my-2" />
+      <Suspense fallback={<ActivityList.Skeleton />}>
+        <ActivityList />
+      </Suspense>
+    </div>
+  );
+};
+
+export default ActivityPage;
